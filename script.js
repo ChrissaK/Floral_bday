bf6dfc926558906f5293ada6a0a4ad47b2aba243
ChrissaK <chrissa.kaza@gmail.com>
2018-11-21 22:09:11 +0000

    Added html, css and js files for the card

---
+++ script.js
@@ -0,0 +1,35 @@
+// Code for the flower to follow mouse cursor
+var flower = document.getElementById("flower");
+document.addEventListener("mousemove", getMouse);
+
+
+flower.style.position = "absolute"; //css
+var flowerpos = {x:0, y:0};
+
+setInterval(followMouse, 50); //executes function followMouse every 50 msec
+
+var mouse = {x:0, y:0}; //mouse.x, mouse.y
+
+function getMouse(el){
+  mouse.x = el.pageX;
+  mouse.y = el.pageY;
+}
+
+function followMouse(){
+  //1. find distance X , distance Y
+  var distX = mouse.x - flowerpos.x;
+  var distY = mouse.y - flowerpos.y;
+  //Easing motion
+   //Progressive reduction of distance
+  flowerpos.x += distX/2;
+  flowerpos.y += distY/2;
+
+  flower.style.left = flowerpos.x + "px";
+  flower.style.top = flowerpos.y + "px";
+}
+//
+// // var surprise_button = document.getElementById("SurpriseBtn");
+// // surprise_button.addEventListener("click",function(){
+// //   console.log("Mpla");
+// //   window.open("card.html");
+// // }, false);
