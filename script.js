8b9eabd2fba5f50f8c5fa8da387e43ba3dfcdb4c
ChrissaK <chrissa.kaza@gmail.com>
2018-11-22 16:05:26 +0000

    added touch events

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Code for the flower to follow mouse cursor
 var flower = document.getElementById("flower");
 document.addEventListener("mousemove", getMouse);
+document.addEventListener("touchstart", getTouchPos);
 
 
 flower.style.position = "absolute"; //css
@@ -8,17 +9,21 @@ var flowerpos = {x:0, y:0};
 
 setInterval(followMouse, 50); //executes function followMouse every 50 msec
 
-var mouse = {x:0, y:0}; //mouse.x, mouse.y
+var client = {x:0, y:0}; //mouse.x, mouse.y
 
 function getMouse(el){ //gets the mouse coordinates on the page
-  mouse.x = el.pageX;
-  mouse.y = el.pageY;
+  client.x = el.pageX;
+  client.y = el.pageY;
+}
+function getTouchPos(e){
+  client.x = e.touches[0].clientX;
+  client.y = e.touches[0].clientY;
 }
 
 function followMouse(){ //updates flower position so as to follow the mouse
   //1. find distance X , distance Y
-  var distX = mouse.x - flowerpos.x;
-  var distY = mouse.y - flowerpos.y;
+  var distX = client.x - flowerpos.x;
+  var distY = client.y - flowerpos.y;
   //Easing motion
    //Progressive reduction of distance
   flowerpos.x += distX/2;
